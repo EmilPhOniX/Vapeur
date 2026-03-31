c7388826e32c0b4b29c9daa932a7d2398bbe2a94
Emilien <emilien.derobert@gmail.com>
2026-03-31 18:24:53 +0200

    chore: mise à jour des packages

--- prisma/migrations/20260331152916/migration.sql
+++ prisma/migrations/20260331152916/migration.sql
@@ -21,11 +21,3 @@ CREATE TABLE "EditeursDeJeux" (
     "idEditeur" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "editeur" TEXT NOT NULL
 );
-
-INSERT INTO "GenreDeJeux" ("idGenre", "genre") VALUES
-  (1, 'Action'),
-  (2, 'Aventure'),
-  (3, 'RPG'),
-  (4, 'Simulation'),
-  (5, 'Sport'),
-  (6, 'MMORPG');
